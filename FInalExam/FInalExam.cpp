e2ab14e88fcac2379702830167a95573725f4f1b
janBogovic <jan.bogovic@live.unic.ac.cy>
2022-06-01 13:21:25 +0300

    addded PutDataInMatrix( puts data that the user gives into matrix), and added printMatrix function that prints the matrix out.

--- FInalExam/FInalExam.cpp
+++ FInalExam/FInalExam.cpp
@@ -9,9 +9,20 @@
 
 
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 int choice;
+double matrix[4][3];
+double UserInputData;
+
+
+
+
+void putDataInMatrix(double UserInputdata);
+
+void printMatrix();
+
 
 /**
 * Function <code>main</code> main function that will run the code
@@ -19,6 +30,7 @@ int choice;
 */
 int main()
 {
+
 	cout << "\n 1) Enter data in matrix (range -5.5 to 5.5) ";
 	cout << "\n 2) Negate all elements (i.e. if an element is -5 => 5, or element is 5 => -5) ";
 	cout << "\n 3) Show data in matrix ";
@@ -32,18 +44,69 @@ int main()
 		{
 		case 1:
 			cout << "Hello World!\n";
+			
 			break;
 		case 2:
 			cout << "Hello World!\n";
 			break;
 		case 3:
-			cout << "Hello World!\n";
+			putDataInMatrix(UserInputData);
+			printMatrix();
 			break;
 		default:
+			cout << "\n Please enter a valid choice! \n";
 			break;
 		}
 		
 
 	} while (choice != 4);
+
+}
+
+
+/**
+* Function <code>putDataInMatrix</code> prints out all the data in the matrix
+* <BR>
+*/
+void printMatrix()
+{
+	for (int i = 0; i < 4; i++)
+	{
+
+		for (int j = 0; j < 3; j++)
+		{
+			cout << "[ " << matrix[0][j] << " ]";
+		}
+		cout << "[ " << matrix[i][0] << " ]" << "\n";
+	}
+}
+
+
+/**
+* Function <code>putDataInMatrix</code> prompts the user to imput the nessecary data in the matrix
+* <BR>
+*/
+void putDataInMatrix(double UserInputdata)
+{
+	
+	for (int i = 0; i < 4; i++)
+	{
+
+		for (int j = 0; j < 3; j++)
+		{
+			cout << "\n Please input value for matrix in postition of " << "[ " << i << " ]" << " [ " << j << " ] : ";
+			cin >> UserInputdata;
+			if (UserInputdata > 5.5 || UserInputdata < -5.5)
+			{
+				cout << "Please input value less that 5.5 or bigger than -5.5 \n";
+				j = j - 1;
+			}
+			else
+			{
+				matrix[i][j] = UserInputdata;
+
+			}
+		}
+	}
 }
 
