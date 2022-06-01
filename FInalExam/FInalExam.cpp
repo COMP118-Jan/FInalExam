173013f3b4230191d72f04e1545eda85701c168a
janBogovic <jan.bogovic@live.unic.ac.cy>
2022-06-01 12:26:26 +0300

    added main menu to FInalExam.cpp(without funcionality)

--- FInalExam/FInalExam.cpp
+++ FInalExam/FInalExam.cpp
@@ -11,27 +11,39 @@
 #include <iostream>
 using namespace std;
 
-
+int choice;
 
 /**
-* Function <code>main</code> main function that will rund the code
+* Function <code>main</code> main function that will run the code
 * <BR>
-* @param 
-* @param 
-* @return 
 */
 int main()
 {
-    cout << "Hello World!\n";
+	cout << "\n 1) Enter data in matrix (range -5.5 to 5.5) ";
+	cout << "\n 2) Negate all elements (i.e. if an element is -5 => 5, or element is 5 => -5) ";
+	cout << "\n 3) Show data in matrix ";
+	cout << "\n 4) Exit ";
+
+	do
+	{
+		cout << "\n Please enter a choice: ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			cout << "Hello World!\n";
+			break;
+		case 2:
+			cout << "Hello World!\n";
+			break;
+		case 3:
+			cout << "Hello World!\n";
+			break;
+		default:
+			break;
+		}
+		
+
+	} while (choice != 4);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
